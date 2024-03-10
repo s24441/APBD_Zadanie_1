f1e9852a1e20d6b7af941c19490affb6ca0fd4f9
Andrzej Michalecki <s24441@pjwstk.edu.pl>
2024-03-10 20:05:32 +0100

    iteration 1..9 added

--- APBD/Task1/Program.cs
+++ APBD/Task1/Program.cs
@@ -4,6 +4,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
+        var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        foreach (var i in list)
+            Console.WriteLine(i);
     }
 }
