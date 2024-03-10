dbe8e31d594f034930cc7a57aaaf098c5179521a
Andrzej Michalecki <s24441@pjwstk.edu.pl>
2024-03-10 20:01:40 +0100

    created Program structure

--- APBD/Task1/Program.cs
+++ APBD/Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace APBDTask1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
