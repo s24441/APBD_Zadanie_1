159f657910833e0ef61bf55655342ed7f4a00462
Andrzej Michalecki <s24441@pjwstk.edu.pl>
2024-03-10 20:12:13 +0100

    end info added to Program

--- APBD/Task1/Program.cs
+++ APBD/Task1/Program.cs
@@ -7,5 +7,7 @@ public class Program
         var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         foreach (var i in list)
             Console.WriteLine(i);
+
+        Console.WriteLine("Program end");
     }
 }
